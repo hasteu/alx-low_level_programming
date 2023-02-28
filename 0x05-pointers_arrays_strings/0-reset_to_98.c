0b05da48b45b4e88bab0f8b1f8d58d260ed85f48
Charaf <hasteu8@gmail.com>
2023-02-27 17:52:35 -0800

    update ex0 to betty standard

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
-/*
-takes a pointer to an int as parameter and 
+/**
+takes a pointer to an int as parameter and
 updates the value it points to to 98
 */
 
 void reset_to_98(int *n)
 {
-        *n = 98;
+	*n = 98;
 }
