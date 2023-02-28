d3ff622d66156c7ed114d78fa0ad68c22713a1ba
Charaf <hasteu8@gmail.com>
2023-02-27 17:55:15 -0800

    update ex0 to betty standard attempt 2

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,9 @@
 #include "main.h"
+
 /**
-takes a pointer to an int as parameter and
-updates the value it points to to 98
+* reset_to_98 - updates the value of the variable
+* the pointer points to to 98
+* @n: pointer to the variable to update
 */
 
 void reset_to_98(int *n)
